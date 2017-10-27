e4b245c35a0b9b1880b3f8e80e69f7ac303396df
user <user@users-MacBook-Air-2.local>
2017-10-27 14:16:11 +0300

    ethersong

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# listen_blockchain
-Listen to the blockchain. The sound you hear is a raw conversion of latest Ethereum transactions into wav file.
+# EtherSong
+Listen to the blockchain. The sound you hear is a raw conversion of latest Ethereum transactions into audiostream.
+
+If you want to run this project local, clone it, provide url for ethereum node in `index.html` and enjoy :)
---
+++ script.js
@@ -0,0 +1,143 @@
+var visualWidth = 318,
+  visualHeight = 100,
+  audioCtx = new window.AudioContext(),
+  analyser = audioCtx.createAnalyser();
+
+analyser.fftSize = 2048;
+analyser.minDecibels = -90;
+analyser.maxDecibels = 0;
+
+var bufferLength = analyser.frequencyBinCount,
+  frequencyData = new Uint8Array(bufferLength);
+
+var bars = [];
+
+for (var i = 1; i <= 128; i++) {
+  bars.push(document.getElementById('bar-' + i));
+}
+
+var ScaleBar = {
+  min: 10,
+  max: visualHeight,
+  sum: 0,
+  get: function (fromMin, fromMax, valueIn) {
+    var toMin = ScaleBar.min,
+      toMax = ScaleBar.max;
+    var result = ((toMax - toMin) * (valueIn - fromMin)) / (fromMax - fromMin) + toMin;
+    return result;
+  }
+};
+
+var MusicVisuals = {
+  call: null,
+  start: function () {
+    analyser.getByteFrequencyData(frequencyData);
+
+    var barcc = 0;
+    var numberOfBars = 128;
+    var steps = numberOfBars;
+
+    var min = Math.min.apply(Math, frequencyData);
+    var max = Math.max.apply(Math, frequencyData);
+    var k = max - min;
+
+
+    for (var i = 0; i < numberOfBars * 2; i += 2) {
+      var y = frequencyData[i];
+      y = (y - min ) / k * 8 + .5;
+
+      barcc++;
+
+      if (barcc > numberOfBars) {
+        barcc = 0;
+      }
+
+      var bar = bars[barcc];
+
+      if (bar) {
+        bar.style.transform = "scaleY(" + y + ')';
+      }
+    }
+
+    MusicVisuals.call = requestAnimationFrame(MusicVisuals.start);
+  }
+};
+
+
+MusicVisuals.start();
+
+
+let k = 4430000;
+let bufferSize = 4096;
+let whiteNoise = audioCtx.createScriptProcessor(bufferSize, 1, 1);
+
+let last_data = [];
+let last_check = 0;
+
+whiteNoise.onaudioprocess = function (e) {
+  let output = e.outputBuffer.getChannelData(0);
+  web3.eth.getBlock(k, function (error, data) {
+    if (Date.now() - last_check < 500) return;
+    console.log('fetched block #', k, 'at', new Date());
+    last_check = Date.now();
+    if (data === null) return;
+    k++;
+    if (data.transactions.length) {
+      let promises = data.transactions.map(txhash => new Promise(resolve =>
+        web3.eth.getTransaction(txhash, function (err, data) {
+          if (err) return resolve([]);
+          let arr = parseHexString(data.input);
+          arr = arr.reduce((prev, curr) => prev.concat(longToByteArray(curr)), []);
+          arr = arr.map(x => x / 255 / 2);
+          resolve(arr);
+        })
+      ));
+
+      Promise.all(promises).then(result => {
+        let arr = result.reduce((prev, curr) => prev.concat(curr), []);
+        last_data = [];
+        for (let i = 0; i < arr.length; i++) {
+          output[i] = arr[i];
+          last_data[i] = arr[i];
+        }
+      });
+    }
+  });
+};
+
+
+whiteNoise.connect(audioCtx.destination);
+whiteNoise.connect(analyser);
+
+function parseHexString(str) {
+  let result = [];
+  while (str.length >= 8) {
+    result.push(parseInt(str.substring(0, 8), 16));
+
+    str = str.substring(8, str.length);
+  }
+
+  return result;
+}
+
+longToByteArray = function (long) {
+  let byteArray = [0, 0, 0, 0, 0, 0, 0, 0];
+
+  for (let index = 0; index < byteArray.length; index++) {
+    let byte = long & 0xff;
+    byteArray [index] = byte;
+    long = (long - byte) / 256;
+  }
+
+  return byteArray;
+};
+
+
+
+
+
+
+
+
+
+
