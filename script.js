2432b48e48234c914ddf10d071922b191aa9371b
telegraphcoin <dev@telegraphcoin.com>
2017-11-08 12:47:47 +0300

    safari fix

--- script.js
+++ script.js
@@ -13,42 +13,19 @@ playBtn.addEventListener('click', function () {
 
   playBlock();
   startSong();
-
 });
 
 function startSong() {
-  var visualWidth = 318,
-    visualHeight = 100;
-
-  // analyser.fftSize = 2048;
-  // analyser.minDecibels = -90;
-  // analyser.maxDecibels = 0;
-
-  var bufferLength = analyser.frequencyBinCount,
-    frequencyData = new Uint8Array(bufferLength);
-
   var bars = [];
 
   for (var i = 1; i <= 128; i++) {
     bars.push(document.getElementById('bar-' + i));
   }
 
-  var ScaleBar = {
-    min: 10,
-    max: visualHeight,
-    sum: 0,
-    get: function (fromMin, fromMax, valueIn) {
-      var toMin = ScaleBar.min,
-        toMax = ScaleBar.max;
-      var result = ((toMax - toMin) * (valueIn - fromMin)) / (fromMax - fromMin) + toMin;
-      return result;
-    }
-  };
-
   var MusicVisuals = {
     call: null,
     start: function () {
-      frequencyData = analyser.getValue();
+      var frequencyData = analyser.getValue();
       var numberOfBars = 128;
 
       var min = Math.min.apply(Math, frequencyData);
@@ -59,7 +36,6 @@ function startSong() {
         let y = frequencyData[64 + i];
         y = (y - min) / k * 8 + .5;
         y *= 1 + (numberOfBars / 2 - i) / 32;
-        // console.log(max, min, k, frequencyData[i], y);
         bars[i].style.transform = "scaleY(" + y + ')';
         bars[numberOfBars - i - 1].style.transform = "scaleY(" + y + ')';
       }
@@ -70,8 +46,5 @@ function startSong() {
 
 
   MusicVisuals.start();
-
-  // whiteNoise.connect(audioCtx.destination);
-  // whiteNoise.connect(analyser);
 }
 
